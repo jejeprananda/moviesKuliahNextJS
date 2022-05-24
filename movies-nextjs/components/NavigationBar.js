29c660d49d6fba3fb7301c61cd146ee55da36201
Jessy Prananda Ismail <jessy.prananda@gmail.com>
2022-05-25 01:24:06 +0700

    firsy commit

---
+++ movies-nextjs/components/NavigationBar.js
@@ -0,0 +1,13 @@
+import { Navbar,Nav,Container } from "react-bootstrap"
+const NavigationBar = () => {
+   return(
+       <Navbar>
+           <Navbar.Brand>Jayakarta Movies</Navbar.Brand>
+           <Nav>
+               <Nav.Link>Trending</Nav.Link>
+               <Nav.Link>Superhero</Nav.Link>
+           </Nav>
+       </Navbar>
+   )
+}
+export default NavigationBar
---
+++ movies-nextjs/pages/index.js
@@ -0,0 +1,31 @@
+import Head from 'next/head'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useRef } from 'react'
+import NavigationBar from '../components/Navigationbar';
+import Intro from '../components/Intro';
+import Trending from '../components/Trending';
+import { Container, Row,Col } from 'react-bootstrap';
+const scrollToRef = (ref) => window.scrollTo(0, ref.current.offsetTop)  
+
+export default function Home() {
+  const myRef = useRef(null)
+  const executeScroll = () => scrollToRef(myRef)
+ 
+ return (
+   <div className='htmlAll'>
+     <Head>
+       <title>Movie List</title>
+       <meta name="description" content="Movie List" />
+       <link rel="icon" href="/favicon.ico" />
+     </Head>
+     <div className='myBG'>
+       <NavigationBar></NavigationBar>
+       <Intro scroll = {executeScroll}></Intro>
+     </div>
+    
+      <div className='trending' ref={myRef}>
+         <Trending></Trending>
+      </div>
+   </div>
+ )
+}
